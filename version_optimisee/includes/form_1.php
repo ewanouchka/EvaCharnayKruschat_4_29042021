d8cc41217317c3d299f2b0bba3f3fd9d969c35b1
ewanouchka <ewanouchka@gmail.com>
2021-05-09 11:37:14 +0200

    suppression success-msg fail-msg pour validation w3c

--- version_optimisee/includes/form_1.php
+++ version_optimisee/includes/form_1.php
@@ -16,7 +16,7 @@
 	$email_body = "You have received a new message. \n\n".
 				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: contact@yoursite.com\n";
+	$headers .= "From: heart_like_a_wheel@hotmail.fr\n";
 	$headers .= "Reply-To: $input_504";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message

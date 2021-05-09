2d7609214b5c39b98bd71f6a01e0b936772c1528
ewanouchka <ewanouchka@gmail.com>
2021-05-09 14:57:07 +0200

    correction adresse mail formulaire

--- version_optimisee/includes/form_1.php
+++ version_optimisee/includes/form_1.php
@@ -9,14 +9,14 @@
 	$input_504 = $_POST['input_504'];
 	$message = $_POST['message'];
 	
-	$to = 'heart_like_a_wheel@hotmail.fr'; // Email submissions are sent to this email
+	$to = 'contact@lachouetteagence.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from your website";
-	$email_body = "You have received a new message.".
-				  "Name: $name Email: $email Input_504: $input_504 Message: $message";
-	$headers = "MIME-Version: 1.0 Content-type: text/plain; charset=UTF-8";	
-	$headers .= "From: contact@lachouetteagence.com";
+	$email_body = "You have received a new message. \n\n".
+				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
+	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+	$headers .= "From: contact@lachouetteagence.com\n";
 	$headers .= "Reply-To: $input_504";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
